b2151fbf8873c7c4a07db7f7ab1fb670e8769222
Nelfimov <nelfimov@mail.ru>
2021-05-08 14:37:51 +0300

    heroku config

--- eshop/settings.py
+++ eshop/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # FOR HEROKU
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra lookup directories for collectstatic to find static files
